349a578b6a202934ab4154bc984e1bdf8008f023
Yun <yun160308@gmail.com>
2019-05-15 17:19:57 +0800

    feat(ALL): 修改readme 增加相关功能

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # uproxy
 系统代理快捷设置工具
 
-##### uproxy -p add -http http://127.0.0.1:1087 -http http://127.0.0.1:1087
-##### uproxy -p remove
+###### add proxy
+##### uproxy -a -http http://127.0.0.1:1087 -http http://127.0.0.1:1087
+###### remove proxy
+##### uproxy -r remove
+###### show proxy config
+##### uproxy -s show
 
 ###### -http http代理地址 (default "http://127.0.0.1:1087")
 ###### -http  https代理地址 (default "http://127.0.0.1:1087")
-###### -p 操作类型 add:增加代理 remove:移除代理 (default "add")
--- uproxy/main.go
+++ uproxy/main.go
@@ -12,63 +12,75 @@ import (
 )
 
 const EXPORT = "export"
-const HTTP_PROXY = "http_proxy"
-const HTTPS_PROXY = "https_proxy"
+const HttpProxy = "http_proxy"
+const HttpsProxy = "https_proxy"
 
 //环境变量文件名称
-const BASH_PROFILE_FILE_NAME = ".bash_profile"
+const BashProfileFileName = ".bash_profile"
 
 //环境变量文件全路径
-var BASH_PROFILE_PATH string
-//操作类型 a:增加代理 r:移除代理
-var processType string
+var BashProfilePath string
+
 //http代理地址
 var httpProxy string
+
 //https代理地址
 var httpsProxy string
 
 //http proxy环境变量设置
-var EXPORT_HTTP_PROXY string
-//https proxy环境变量设置
-var EXPORT_HTTPS_PROXY string
+var ExportHttpProxy string
 
-func cmd() {
-	flag.StringVar(&processType, "p", "add", "操作类型 add:增加代理 remove:移除代理")
+//https proxy环境变量设置
+var ExportHttpsProxy string
+
+//add proxy
+var a bool
+
+//remove proxy
+var r bool
+
+//show proxy config
+var s bool
+
+func init() {
+	flag.BoolVar(&a, "a", false, "add:增加代理")
+	flag.BoolVar(&r, "r", false, "remove:移除代理")
+	flag.BoolVar(&s, "s", false, "show:显示代理配置")
 	flag.StringVar(&httpProxy, "http", "http://127.0.0.1:1087", "http代理地址")
 	flag.StringVar(&httpsProxy, "https", "http://127.0.0.1:1087", "https代理地址")
 	flag.Parse()
 }
 
 func main() {
-	cmd()
 	userHome, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	BASH_PROFILE_PATH = userHome + "/" + BASH_PROFILE_FILE_NAME
-	EXPORT_HTTP_PROXY = EXPORT + " " + HTTP_PROXY + "=" + httpProxy + "\n"
-	EXPORT_HTTPS_PROXY = EXPORT + " " + HTTPS_PROXY + "=" + httpsProxy + "\n"
-	switch processType {
-	case "add":
-		//增加代理
+	BashProfilePath = userHome + "/" + BashProfileFileName
+	ExportHttpProxy = EXPORT + " " + HttpProxy + "=" + httpProxy + "\n"
+	ExportHttpsProxy = EXPORT + " " + HttpsProxy + "=" + httpsProxy + "\n"
+	//增加代理
+	if a {
 		addProxy()
-		break
-	case "remove":
-		//移除代理
-		removeProxy()
-		break
-	default:
-		//参数错误
-		fmt.Println("参数错误 -p a:增加代理 r:移除代理")
 	}
-
+	//移除代理
+	if r {
+		removeProxy()
+	}
+	//显示代理配置
+	if s {
+		showProxyConfig()
+	}
+	if !a && !r && !s {
+		fmt.Println("-？ for help")
+	}
 }
 
 ///增加代理
 func addProxy() {
 	//追加写入
-	f, err := os.OpenFile(BASH_PROFILE_PATH, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	f, err := os.OpenFile(BashProfilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -78,20 +90,23 @@ func addProxy() {
 		panic(err)
 	}
 	content := string(b)
-	if strings.Contains(content, HTTP_PROXY) || strings.Contains(content, HTTPS_PROXY) {
+	if strings.Contains(content, HttpProxy) || strings.Contains(content, HttpsProxy) {
+		showProxyConfig()
 		fmt.Println("代理设置已经存在")
 		return
 	}
-	_, err = io.WriteString(f, EXPORT_HTTP_PROXY)
+	_, err = io.WriteString(f, ExportHttpProxy)
 	if err != nil {
 		panic(err)
 	}
-	_, err = io.WriteString(f, EXPORT_HTTPS_PROXY)
+	_, err = io.WriteString(f, ExportHttpsProxy)
 	if err != nil {
 		panic(err)
 	}
-	cmdSuccess := execCommand("source", []string{BASH_PROFILE_PATH})
+	cmdSuccess := execCommand("source", []string{BashProfilePath})
 	if cmdSuccess {
+		fmt.Print(ExportHttpProxy)
+		fmt.Print(ExportHttpsProxy)
 		fmt.Println("执行成功")
 	} else {
 		fmt.Println("执行失败")
@@ -100,13 +115,13 @@ func addProxy() {
 
 ///移除代理
 func removeProxy() {
-	exist := checkFileIsExist(BASH_PROFILE_PATH)
+	exist := checkFileIsExist(BashProfilePath)
 	if !exist {
 		fmt.Println("代理设置不存在")
 		return
 	}
 	//覆盖写入
-	f, err := os.OpenFile(BASH_PROFILE_PATH, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, os.ModePerm)
+	f, err := os.OpenFile(BashProfilePath, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
@@ -118,7 +133,7 @@ func removeProxy() {
 		if err != nil || io.EOF == err {
 			break
 		}
-		if strings.Contains(line, HTTP_PROXY) || strings.Contains(line, HTTPS_PROXY) {
+		if strings.Contains(line, HttpProxy) || strings.Contains(line, HttpsProxy) {
 			continue
 		}
 		content = content + line
@@ -127,11 +142,41 @@ func removeProxy() {
 	if err != nil {
 		panic(err)
 	}
-	cmdSuccess := execCommand("source", []string{BASH_PROFILE_PATH})
+	cmdSuccess := execCommand("source", []string{BashProfilePath})
 	if cmdSuccess {
 		fmt.Println("执行成功")
 	} else {
 		fmt.Println("执行失败")
+	}
+}
+
+///显示代理配置
+func showProxyConfig() {
+	exist := checkFileIsExist(BashProfilePath)
+	if !exist {
+		fmt.Println("代理设置不存在")
+		return
+	}
+	//读取文件
+	f, err := os.OpenFile(BashProfilePath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	var hasProxyConfig = false
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+		if strings.Contains(line, HttpProxy) || strings.Contains(line, HttpsProxy) {
+			fmt.Println(line)
+			hasProxyConfig = true
+		}
+	}
+	if !hasProxyConfig {
+		fmt.Println("代理设置不存在")
 	}
 }
 
